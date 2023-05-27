674650547d571b6e6d7e28d57b323d1da1b0f0bc
muideen7 <olayeyeayomide2000@gmail.com>
2023-05-27 16:38:32 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,28 +1,13 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
+# Increases the amount of traffic an Nginx server can handle.
 
-# Configure Nginx server
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('my_module/nginx.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-# Increase file limit for Nginx
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => "ULIMIT='-n 4096'",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx service when configuration changes
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-  subscribe => File['/etc/nginx/nginx.conf', '/etc/default/nginx'],
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
