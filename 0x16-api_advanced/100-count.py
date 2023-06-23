e817d357dc6ebf99f88b66db3c55f0af0d2b8a98
muideen7 <olayeyeayomide2000@gmail.com>
2023-06-23 20:50:18 +0100

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,52 @@
 #!/usr/bin/python3
-"""queries the Reddit API, parses the title of all hot articles,
-and prints a sorted count of given keywords"""
-
-import re
+"""A script for counting hot terms on subreddits"""
 import requests
 
 
-def count_words(subreddit, word_list, word_dict={},
-                after='', count=0, not_first=0):
-    """queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords"""
+def count_words(subreddit, word_list, after=None, count={}):
+    """
+    a recursive function that queries the Reddit API,
+    parses the title of all hot articles, and prints a
+    sorted count of given keywords (case-insensitive,
+    delimited by spaces. Javascript should count as javascript,
+    but java should not).
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 12; SM-S906N\
-               Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML,\
-               like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile\
-               Safari/537.36'
-               }
-    parameters = {
-        'after': after,
-        'count': count,
-        'limit': 100
-    }
-    try:
-        data = requests.get(
-            'https://www.reddit.com/r/{}/hot/.json'.format(subreddit),
-            headers=headers, params=parameters, allow_redirects=False).json()
-        [filter_word(word_list, word_dict, post['data']['title'])
-         for post in data['data']["children"]]
-
-        if data['data']['after']:
-            count_words(subreddit, word_list, word_dict,
-                        after=data['data']['after'],
-                        count=count+data['data']['dist'], not_first=1)
-            if not_first == 0:
-                for key, value in sorted(word_dict.items(),
-                                         key=lambda val: (-val[1], val[0])):
-                    print('{}:'.format(key), value)
-    except Exception:
+    Parameters:
+        subreddit - the subreddit to search
+        word_list - contains the same word (case-insensitive),
+            the final count should be the sum of each duplicate
+    """
+    if word_list == []:
         return None
-
-
-def filter_word(word_list, word_dict, string):
-    """filter words count number of words and all it to total in word_dict"""
-    word_list = [word.lower() for word in word_list]
-
+    else:
+        lower_list = (map(lambda word: word.lower(), word_list))
+        word_list = list(lower_list)
+    if after is None:
+        hot = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    else:
+        hot = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+            subreddit, after)
+    hot_request = requests.get(hot,
+                               headers={"user-agent": "user"},
+                               allow_redirects=False)
+    try:
+        data = hot_request.json().get("data")
+    except:
+        return
     for word in word_list:
-        reg_string = r'\b{}\b'.format(word)
-        word_count = len(re.findall(reg_string, string, flags=re.IGNORECASE))
-        if word_count > 0:
-            if word_dict.get(word, None) is None:
-                word_dict[word] = word_count
-            else:
-                word_dict[word] = word_dict[word] + word_count
+        if word not in count.keys():
+            count[word] = 0
+    children = data.get("children")
+    for child in children:
+        title = (child.get("data").get("title").lower())
+        title = title.split(' ')
+        for word in word_list:
+            count[word] += title.count(word)
+    after = data.get("after")
+    if after is not None:
+        return count_words(subreddit, word_list, after, count)
+    else:
+        sorted_subs = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+        for i in sorted_subs:
+            if i[1] != 0:
+                print(i[0] + ": " + str(i[1]))
